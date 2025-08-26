7baf4aeeecee99dbbbcfc8d06eb3b4b8120bf666
Mohammad-code313 <safaytulhoqa@gmail.com>
2025-08-26 18:34:53 +0600

    Add files via upload
    added pass task

---
+++ pass/hello/HelloApp/HelloApp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Welcome to the HelloApp!" << std::endl;
+    std::cout << "Press Enter to exit...";
+    std::cin.get();
+    return 0;
+}
